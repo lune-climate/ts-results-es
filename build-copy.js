600c8768e225e3a327872ede0c09e151d4c33aeb
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-08-05 12:42:24 -0500

    better type resolution

---
+++ build-copy.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const resolveJson = (specifier) => {
+    if(specifier.startsWith('cjs')) {
+        return { type: 'commonjs' }
+    }
+    return { type: 'module' }
+}
+
+
+const paths = {
+    'cjs' : path.join(__dirname, 'dist', 'cjs', 'package.json'),
+    'esm': path.join(__dirname, 'dist', 'esm', 'package.json'),
+    'cjs/rxjs-operators': path.join(__dirname, 'dist', 'cjs', 'rxjs-operators', 'package.json'),
+    'esm/rxjs-operators': path.join(__dirname, 'dist', 'esm', 'rxjs-operators', 'package.json')
+}
+const entries = Object.entries(paths);
+
+const specifierToJson = Object
+    .fromEntries(entries
+        .map(([specifier, _]) => [specifier, JSON.stringify(resolveJson(specifier))]));
+
+for(const [specifier, fullpath] of entries) {
+    // Create the dist/esm directory if it doesn't exist
+    if (!fs.existsSync(path.dirname(fullpath))) {
+      fs.mkdirSync(path.dirname(fullpath), { recursive: true });
+    }
+    fs.writeFileSync(fullpath , specifierToJson[specifier], null, 2);
+}
+
+// Create the dist/esm directory if it doesn't exist
+// Write the JSON content to the file
+
+console.log('JSONs copied to ' + entries.map(k => k[0]));
